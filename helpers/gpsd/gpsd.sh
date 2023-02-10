6b1180f727bc203b7172acf64e9f113a537ee390
Jose Nuñez <jnunyez@users.noreply.github.com>
2023-02-10 22:20:35 +0100

    add passive mode

--- helpers/gpsd/gpsd.sh
+++ helpers/gpsd/gpsd.sh
@@ -5,6 +5,6 @@
 # BUS=`grep PCI_SLOT_NAME /sys/class/net/${ETH}/device/uevent | cut -c 20- | head -c 5 | sed 's/://'
 # BUS is in the bash environment
 
-echo "GNSS Device : ttyGNSS_"${BUS}
+echo "GNSS Device : "${BUS}
 
-/usr/local/sbin/gpsd -n -S 2947 -G -N -D 5 /dev/ttyGNSS_${BUS}
+/usr/local/sbin/gpsd -p -n -S 2947 -G -N -D 5 /dev/${BUS}
