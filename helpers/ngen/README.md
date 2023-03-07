7cbe4fc1f90565808fb9f20bb0102f4f2017e7a0
Jose Nuñez <jnunyez@users.noreply.github.com>
2023-03-07 17:07:56 +0100

    polished PTP NGEN KPIs, decoupled calculation from collecting test report

---
+++ helpers/ngen/README.md
@@ -0,0 +1,69 @@
+# QuickStart
+
+
+## G.8272.2 Noise Generation KPIs
+
+
+```console
+# python3 ptp_ngen_kpis.py -h
+usage: ptp_ngen_kpis.py [-h] -i INPUT [-c CLOCKCLASS] [-t TRANSIENT] [--plot PLOT] [-o OUTPUT]
+
+Process ptp4l samples to calculate NGEN KPIs.
+
+options:
+  -h, --help            show this help message and exit
+  -c CLOCKCLASS, --clockclass CLOCKCLASS
+                        clock class-[C,D] requirement to satisfy, defaults to Class-C
+  -t TRANSIENT, --transient TRANSIENT
+                        transient period, defaults to 300sec
+  --plot PLOT           add plots to the results
+  -o OUTPUT, --output OUTPUT
+                        Output file name, defaults to stdout
+
+required arguments:
+  -i INPUT, --input INPUT
+                        Input sample data
+```
+
+## Build 
+
+1) Build the image to calculate Noise Generation KPIs according to G.8273.2:
+
+```
+# podman build -t quay.io/jnunez/ngen_kpis:0.1 -f ./ContainerFile
+```
+
+## Run
+
+1) Pull the G.8273.2 Noise Generation KPIs container image:
+
+```
+# podman pull quay.io/jnunez/ngen_kpis:0.1
+```
+
+2) Data File derivated from ptp4l messages:
+
+```
+tstamp,phase,state,freq,delay,event
+1184958.065,,,,,"port 1: UNCALIBRATED to SLAVE on MASTER_CLOCK_SELECTED"
+1184958.128,-98,2,-15910,22273
+1184958.190,-91,2,-15908,22273
+1184958.253,-67,2,-15876,22272
+```
+
+where:
+
+tstamp: Timestamp
+phase: Phase offset in ns
+state: Servo clock state
+freq: Frequency offset
+delay: Path delay
+event: Event information   
+
+3) Run the container to calculate G.8273.2 Noise Generation KPIs consuming post-processed data from ptp4l in `/tmp/data.csv`:
+
+```
+# podman run -it --rm -v /tmp:/tmp quay.io/jnunez/ngen_kpis:0.1 -i /tmp/data.csv
+```
+
+
---
+++ helpers/ngen/preprocess.py
@@ -0,0 +1,60 @@
+import os
+from scipy import signal
+from scipy.signal import butter, filtfilt
+import time
+import pandas as pd
+import numpy as np
+
+
+
+def calculate_update_rate(df,s2_count):
+    """
+    Calculate frequency of reception of new samples from ptp4l process
+
+    Input: samples, number_of_samples
+    Output: update_rate in seconds
+    """
+    firstS2 = (df['state'].values == 2).argmax()
+    prevS2 = firstS2
+    cumS2delta = 0
+    x=0
+    S2count=0    
+    for x in range (firstS2+1, 1024): #use just about 1k samples (minus ~ init S0/S1's and any events)\n",
+        if df.loc[x].state == 2:
+            cumS2delta = cumS2delta+(df.loc[x].tstamp-df.loc[prevS2].tstamp)
+            prevS2=x
+            S2count=S2count+1
+    return round((1/(cumS2delta/S2count)))
+
+def run(df, transient_period):
+    """
+    Preprocess data
+
+    Input: samples
+    Output: lpf, frequency rate and samples in locked state
+    """
+    s2_count = (df['state'].values == 2).sum()
+    print ("number of samples with servo s2 :",s2_count)
+
+    update_rate = calculate_update_rate(df, s2_count)
+    print ("Update rate estimate from S2 deltas: ",update_rate, "updates/s")
+
+    #initial transient sync period is fixed to 5 minutes
+    end_transient_period=transient_period*update_rate
+
+    #input signal after transient sync period
+    input_signal=df.phase[end_transient_period:len(df)]
+
+    fc=0.1               # Cutoff Frequency 0.1Hz low-pass filter
+    w=fc/(update_rate/2) # the critical frequency for digital filters w is normalized from 0 to 1 where 1 is Nyquist freqency.
+    btype='low'          # band type is type of filter
+    fiorder=5            # the order of the filter
+    analog=False         # it is always a digital filter
+    output='ba'          # type of output: b numerator coefficient vector and a is denominator coefficient vector
+
+    b, a = butter(fiorder, w, btype, analog, output)
+
+    lpf_signal = filtfilt(b, a, input_signal)
+
+    return lpf_signal, update_rate, s2_count
+
