e1f2a8e25a4e2fb6e83a6b9711bf770fa9e87e1f
Charlie Wheeler-Robinson <cwheeler@redhat.com>
2023-03-07 12:06:20 -0500

    add clients tests
    Signed-off-by: Charlie Wheeler-Robinson <cwheeler@redhat.com>

---
+++ pkg/clients/clients_test.go
@@ -0,0 +1,42 @@
+package clients_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/redhat-partner-solutions/vse-sync-testsuite/pkg/clients"
+)
+
+const (
+	kubeconfigPath string = "test_files/kubeconfig"
+	notAKubeconfigPath string = ""
+)
+
+var (
+	emptyKubeconfigList []string
+)
+
+var _ = Describe("Client", func() {
+	When("A clientset is requested with no kubeconfig", func() {
+		It("should panic", func() {
+			var clientset *clients.Clientset
+			Expect(func() {clientset = clients.GetClientset(notAKubeconfigPath)}).To(Panic())
+			Expect(clientset).To(BeNil())
+			Expect(func() {clientset = clients.GetClientset(emptyKubeconfigList...)}).To(Panic())
+			Expect(clientset).To(BeNil())
+		})
+	})
+	When("A clientset is requested using a valid kubeconfig", func() {
+		It("should be returned", func() {
+			clientset := clients.GetClientset(kubeconfigPath)
+			Expect(clientset).NotTo(BeNil())
+		})
+	})
+})
+
+func TestCluster(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Clients Suite")
+}
