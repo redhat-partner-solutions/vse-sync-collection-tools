bf1dee90624733c0ddd6a08e1c9462c7b6d9b625
Michele Costa <micosta@redhat.com>
2023-05-25 16:54:14 +0100

    Allow users use a env vars or a config file instead of cli flags

--- pkg/cmd/collect.go
+++ pkg/cmd/collect.go
@@ -13,6 +13,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/redhat-partner-solutions/vse-sync-collection-tools/pkg/collectors"
 	"github.com/redhat-partner-solutions/vse-sync-collection-tools/pkg/runner"
@@ -40,19 +41,50 @@ var (
 	keepDebugFiles         bool
 )
 
+type CollectionParams struct {
+	KubeConfig             string   `mapstructure:"kubeconfig"`
+	PTPInterface           string   `mapstructure:"ptp_interface"`
+	OutputFile             string   `mapstructure:"output_file"`
+	Duration               string   `mapstructure:"duration"`
+	LogsOutputFile         string   `mapstructure:"logs_output"`
+	TempDir                string   `mapstructure:"tempdir"`
+	CollectorNames         []string `mapstructure:"collectors"`
+	PollInterval           int      `mapstructure:"poll_interval"`
+	DevInfoAnnouceInterval int      `mapstructure:"announce_interval"`
+	UseAnalyserJSON        bool     `mapstructure:"use_analyser_format"`
+	IncludeLogTimestamps   bool     `mapstructure:"log_timestamps"`
+	KeepDebugFiles         bool     `mapstructure:"keep_debug_files"`
+}
+
+func (p *CollectionParams) CheckForRequiredFields() error {
+	missing := make([]string, 0)
+	if p.KubeConfig == "" {
+		missing = append(missing, "kubeconfig")
+	}
+	if p.PTPInterface == "" {
+		missing = append(missing, "interface")
+	}
+	if len(missing) > 0 {
+		return fmt.Errorf(`required flag(s) "%s" not set`, strings.Join(missing, `", "`))
+	}
+	return nil
+}
+
 // collectCmd represents the collect command
 var collectCmd = &cobra.Command{
 	Use:   "collect",
 	Short: "Run the collector tool",
 	Long:  `Run the collector tool to gather data from your target cluster`,
 	Run: func(cmd *cobra.Command, args []string) {
-		collectionRunner := runner.NewCollectorRunner(collectorNames)
+		runtimeConfig := &CollectionParams{}
+		err := populateParams(cmd, runtimeConfig)
+		utils.IfErrorExitOrPanic(err)
 
-		requestedDuration, err := time.ParseDuration(requestedDurationStr)
+		requestedDuration, err := time.ParseDuration(runtimeConfig.Duration)
+		utils.IfErrorExitOrPanic(err)
 		if requestedDuration.Nanoseconds() < 0 {
 			log.Panicf("Requested duration must be positive")
 		}
-		utils.IfErrorExitOrPanic(err)
 
 		for _, c := range collectorNames {
 			if (c == collectors.LogsCollectorName || c == runner.All) && logsOutputFile == "" {
@@ -78,18 +110,19 @@ var collectCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 
+		collectionRunner := runner.NewCollectorRunner(runtimeConfig.CollectorNames)
 		collectionRunner.Run(
-			kubeConfig,
-			outputFile,
+			runtimeConfig.KubeConfig,
+			runtimeConfig.OutputFile,
 			requestedDuration,
-			pollInterval,
-			devInfoAnnouceInterval,
-			ptpInterface,
-			useAnalyserJSON,
-			logsOutputFile,
-			includeLogTimestamps,
-			tempDir,
-			keepDebugFiles,
+			runtimeConfig.PollInterval,
+			runtimeConfig.DevInfoAnnouceInterval,
+			runtimeConfig.PTPInterface,
+			runtimeConfig.UseAnalyserJSON,
+			runtimeConfig.LogsOutputFile,
+			runtimeConfig.IncludeLogTimestamps,
+			runtimeConfig.TempDir,
+			runtimeConfig.KeepDebugFiles,
 		)
 	},
 }
@@ -110,6 +143,9 @@ func init() { //nolint:funlen // Allow this to get a little long
 		"A positive duration string sequence of decimal numbers and a unit suffix, such as \"300ms\", \"1.5h\" or \"2h45m\"."+
 			" Valid time units are \"s\", \"m\", \"h\".",
 	)
+	err := viper.BindPFlag("duration", collectCmd.Flags().Lookup("duration"))
+	utils.IfErrorExitOrPanic(err)
+
 	collectCmd.Flags().IntVarP(
 		&pollInterval,
 		"rate",
@@ -118,6 +154,11 @@ func init() { //nolint:funlen // Allow this to get a little long
 		"Poll interval for querying the cluster. The value will be polled once every interval. "+
 			"Using --rate 10 will cause the value to be polled once every 10 seconds",
 	)
+	err = viper.BindPFlag("poll_interval", collectCmd.Flags().Lookup("rate"))
+	utils.IfErrorExitOrPanic(err)
+	viper.RegisterAlias("poll_rate", "poll_interval")
+	viper.RegisterAlias("rate", "poll_interval")
+
 	collectCmd.Flags().IntVarP(
 		&devInfoAnnouceInterval,
 		"announce",
@@ -125,6 +166,11 @@ func init() { //nolint:funlen // Allow this to get a little long
 		defaultDevInfoInterval,
 		"interval at which to emit the device info summary to the targeted output.",
 	)
+	err = viper.BindPFlag("announce_interval", collectCmd.Flags().Lookup("announce"))
+	utils.IfErrorExitOrPanic(err)
+	viper.RegisterAlias("announce_rate", "announce_interval")
+	viper.RegisterAlias("announce", "announce_interval")
+
 	defaultCollectorNames := make([]string, 0)
 	defaultCollectorNames = append(defaultCollectorNames, runner.All)
 	collectCmd.Flags().StringSliceVarP(
@@ -140,19 +186,32 @@ func init() { //nolint:funlen // Allow this to get a little long
 			strings.Join(runner.OptionalCollectorNames, ", "),
 		),
 	)
+	err = viper.BindPFlag("collectors", collectCmd.Flags().Lookup("collector"))
+	utils.IfErrorExitOrPanic(err)
+	viper.RegisterAlias("collector", "collectors")
 
 	collectCmd.Flags().StringVarP(
 		&logsOutputFile,
 		"logs-output", "l", "",
 		"Path to the logs output file. This is required when using the logs collector",
 	)
+	err = viper.BindPFlag("logs_output", collectCmd.Flags().Lookup("logs-output"))
+	utils.IfErrorExitOrPanic(err)
+
 	collectCmd.Flags().BoolVar(
 		&includeLogTimestamps,
 		"log-timestamps", defaultIncludeLogTimestamps,
 		"Specifies if collected logs should include timestamps or not. (default is false)",
 	)
+	err = viper.BindPFlag("log_timestamps", collectCmd.Flags().Lookup("log-timestamps"))
+	utils.IfErrorExitOrPanic(err)
 
 	collectCmd.Flags().StringVarP(&tempDir, "tempdir", "t", defaultTempDir,
 		"Directory for storing temp/debug files. Must exist.")
+	err = viper.BindPFlag("tempdir", collectCmd.Flags().Lookup("tempdir"))
+	utils.IfErrorExitOrPanic(err)
+
 	collectCmd.Flags().BoolVar(&keepDebugFiles, "keep", defaultKeepDebugFiles, "Keep debug files")
+	err = viper.BindPFlag("keep_debug_files", collectCmd.Flags().Lookup("keep"))
+	utils.IfErrorExitOrPanic(err)
 }
--- pkg/cmd/common.go
+++ pkg/cmd/common.go
@@ -3,7 +3,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/redhat-partner-solutions/vse-sync-collection-tools/pkg/utils"
 )
@@ -17,12 +21,15 @@ var (
 
 func AddKubeconfigFlag(targetCmd *cobra.Command) {
 	targetCmd.Flags().StringVarP(&kubeConfig, "kubeconfig", "k", "", "Path to the kubeconfig file")
-	err := targetCmd.MarkFlagRequired("kubeconfig")
+	err := viper.BindPFlag("kubeconfig", targetCmd.Flags().Lookup("kubeconfig"))
 	utils.IfErrorExitOrPanic(err)
 }
 
 func AddOutputFlag(targetCmd *cobra.Command) {
 	targetCmd.Flags().StringVarP(&outputFile, "output", "o", "", "Path to the output file")
+	err := viper.BindPFlag("output_file", targetCmd.Flags().Lookup("output"))
+	utils.IfErrorExitOrPanic(err)
+	viper.RegisterAlias("output", "output_file")
 }
 
 func AddFormatFlag(targetCmd *cobra.Command) {
@@ -33,10 +40,31 @@ func AddFormatFlag(targetCmd *cobra.Command) {
 		false,
 		"Output in a format to be used by analysers from vse-sync-pp",
 	)
+	err := viper.BindPFlag("use_analyser_format", targetCmd.Flags().Lookup("use-analyser-format"))
+	utils.IfErrorExitOrPanic(err)
 }
 
 func AddInterfaceFlag(targetCmd *cobra.Command) {
 	targetCmd.Flags().StringVarP(&ptpInterface, "interface", "i", "", "Name of the PTP interface")
-	err := targetCmd.MarkFlagRequired("interface")
+	err := viper.BindPFlag("ptp_interface", targetCmd.Flags().Lookup("interface"))
 	utils.IfErrorExitOrPanic(err)
+	viper.RegisterAlias("interface", "ptp_interface")
+}
+
+type Params interface {
+	CheckForRequiredFields() error
+}
+
+func populateParams(cmd *cobra.Command, params Params) error {
+	err := viper.Unmarshal(params)
+	utils.IfErrorExitOrPanic(err)
+	err = params.CheckForRequiredFields()
+	if err != nil {
+		cmd.PrintErrln(err.Error())
+		err = cmd.Usage()
+		utils.IfErrorExitOrPanic(err)
+		os.Exit(int(utils.InvalidArgs))
+		return fmt.Errorf("failed to populate params: %w", err)
+	}
+	return nil
 }
