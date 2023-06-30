9f1f8318d812c5681df67dc620d997f868acba0a
Michele Costa <micosta@redhat.com>
2023-06-30 17:13:22 +0100

    Add collect sub command

---
+++ pkg/cmd/collect.go
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/redhat-partner-solutions/vse-sync-testsuite/pkg/runner"
+	"github.com/redhat-partner-solutions/vse-sync-testsuite/pkg/utils"
+)
+
+var (
+	pollCount              int
+	pollInterval           int
+	devInfoAnnouceInterval int
+	collectorNames         []string
+)
+
+// collectCmd represents the collect command
+var collectCmd = &cobra.Command{
+	Use:   "collect",
+	Short: "Run the collector tool",
+	Long:  `Run the collector tool to gather data from your target cluster`,
+	Run: func(cmd *cobra.Command, args []string) {
+		collectionRunner := runner.NewCollectorRunner(collectorNames)
+		collectionRunner.Run(
+			kubeConfig,
+			outputFile,
+			pollCount,
+			pollInterval,
+			devInfoAnnouceInterval,
+			ptpInterface,
+			useAnalyserJSON,
+		)
+	},
+}
+
+func init() { //nolint:funlen // Allow this to get a little long
+	rootCmd.AddCommand(collectCmd)
+
+	collectCmd.PersistentFlags().StringVarP(&kubeConfig, "kubeconfig", "k", "", "Path to the kubeconfig file")
+	err := collectCmd.MarkPersistentFlagRequired("kubeconfig")
+	utils.IfErrorExitOrPanic(err)
+
+	collectCmd.PersistentFlags().StringVarP(&ptpInterface, "interface", "i", "", "Name of the PTP interface")
+	err = collectCmd.MarkPersistentFlagRequired("interface")
+	utils.IfErrorExitOrPanic(err)
+
+	collectCmd.Flags().IntVarP(
+		&pollCount,
+		"count",
+		"c",
+		defaultCount,
+		"Number of queries the cluster (-1) means infinite",
+	)
+	collectCmd.Flags().IntVarP(
+		&pollInterval,
+		"rate",
+		"r",
+		defaultPollInterval,
+		"Poll interval for querying the cluster. The value will be polled once ever interval. "+
+			"Using --rate 10 will cause the value to be polled once every 10 seconds",
+	)
+	collectCmd.Flags().IntVarP(
+		&devInfoAnnouceInterval,
+		"announce",
+		"a",
+		defaultDevInfoInterval,
+		"interval for announcing the dev info",
+	)
+	defaultCollectorNames := make([]string, 0)
+	defaultCollectorNames = append(defaultCollectorNames, runner.All)
+	collectCmd.Flags().StringSliceVarP(
+		&collectorNames,
+		"collector",
+		"s",
+		defaultCollectorNames,
+		fmt.Sprintf(
+			"the collectors you wish to run (case-insensitive):\n"+
+				"\trequired collectors: %s (will be automatically added)\n"+
+				"\toptional collectors: %s",
+			strings.Join(runner.RequiredCollectorNames, ", "),
+			strings.Join(runner.OptionalCollectorNames, ", "),
+		),
+	)
+	collectCmd.PersistentFlags().StringVarP(&outputFile, "output", "o", "", "Path to the output file")
+
+	collectCmd.PersistentFlags().BoolVarP(
+		&useAnalyserJSON,
+		"use-analyser-format",
+		"j",
+		false,
+		"Output in a format to be used by analysers from vse-sync-pp",
+	)
+}
