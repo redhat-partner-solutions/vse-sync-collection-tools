95048a21bcfdafc0ad4db1f24514eca0ae450bfd
Michele Costa <micosta@redhat.com>
2023-07-11 17:05:01 +0100

    Rename verifyEnv command to env verify

--- pkg/cmd/verifyEnv.go
+++ pkg/cmd/verifyEnv.go
@@ -8,18 +8,25 @@ import (
 	"github.com/redhat-partner-solutions/vse-sync-testsuite/pkg/verify"
 )
 
+var envCmd = &cobra.Command{
+	Use:   "env",
+	Short: "environment based actions",
+	Long:  `environment based actions`,
+}
+
 // verifyEnvCmd represents the verifyEnv command
 var verifyEnvCmd = &cobra.Command{
-	Use:   "verifyEnv",
-	Short: "verify the environment for collection",
-	Long:  `verify the environment for collection`,
+	Use:   "verify",
+	Short: "verify the environment is ready for collection",
+	Long:  `verify the environment is ready for collection`,
 	Run: func(cmd *cobra.Command, args []string) {
 		verify.Verify(ptpInterface, kubeConfig, useAnalyserJSON)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(verifyEnvCmd)
+	rootCmd.AddCommand(envCmd)
+	envCmd.AddCommand(verifyEnvCmd)
 	AddKubeconfigFlag(verifyEnvCmd)
 	AddOutputFlag(verifyEnvCmd)
 	AddFormatFlag(verifyEnvCmd)
