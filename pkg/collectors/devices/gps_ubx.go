2d2515c9a0bf64afc5e0f26299585b44bb4613e4
Michele Costa <micosta@redhat.com>
2023-07-13 16:51:28 +0100

    fixup! Add GNSS firmware version

--- pkg/collectors/devices/gps_ubx.go
+++ pkg/collectors/devices/gps_ubx.go
@@ -96,7 +96,7 @@ func init() {
 // processUBXNav parses the output of the ubxtool extracting the required values for gpsNav
 func processUBXNav(result map[string]string) (map[string]any, error) {
 	processedResult := make(map[string]any)
-	match := ubxNavRegex.FindStringSubmatch(result["GPS"])
+	match := ubxFirmwareVersion.FindStringSubmatch(result["GPS"])
 	if len(match) == 0 {
 		return processedResult, fmt.Errorf(
 			"unable to parse UBX Nav Status or Clock from %s",
