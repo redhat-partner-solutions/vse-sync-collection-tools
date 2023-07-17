480703fcf9269b8507d7f45478753e558c328505
Michele Costa <micosta@redhat.com>
2023-07-17 15:39:20 +0100

    Remove GPSD context and only use linuxptp-daemon-container
    The pre-GA PTP operator does not have a seperate container for gpds like our work
    around operator did.

--- pkg/collectors/contexts/contexts.go
+++ pkg/collectors/contexts/contexts.go
@@ -22,11 +22,3 @@ func GetPTPDaemonContext(clientset *clients.Clientset) (clients.ContainerContext
 	}
 	return ctx, nil
 }
-
-func GetPTPgpsdContext(clientset *clients.Clientset) (clients.ContainerContext, error) {
-	ctx, err := clients.NewContainerContext(clientset, PTPNamespace, PTPPodNamePrefix, GPSContainer)
-	if err != nil {
-		return clients.ContainerContext{}, fmt.Errorf("could not create container context %w", err)
-	}
-	return ctx, nil
-}
