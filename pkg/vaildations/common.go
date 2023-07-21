5964eaa2af18ec84509227ca42c1c3e546fd2a50
Michele Costa <micosta@redhat.com>
2023-07-21 10:46:06 +0100

    Make generic version check

---
+++ pkg/vaildations/common.go
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package vaildations
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"golang.org/x/mod/semver"
+
+	"github.com/redhat-partner-solutions/vse-sync-collection-tools/pkg/utils"
+)
+
+type VersionCheck struct {
+	id           string `json:"-"`
+	Version      string `json:"version"`
+	checkVersion string `json:"-"`
+	minVersion   string `json:"-"`
+}
+
+func (verCheck *VersionCheck) Verify() error {
+	ver := fmt.Sprintf("v%s", verCheck.checkVersion)
+	if !semver.IsValid(ver) {
+		return fmt.Errorf("could not parse version %s", ver)
+	}
+	if semver.Compare(ver, fmt.Sprintf("v%s", verCheck.minVersion)) < 0 {
+		return utils.NewInvalidEnvError(
+			fmt.Errorf("invalid version: %s < %s", verCheck.checkVersion, verCheck.minVersion),
+		)
+	}
+	return nil
+}
+
+func (verCheck *VersionCheck) GetID() string {
+	return verCheck.id
+}
+
+func (verCheck *VersionCheck) GetData() any { //nolint:ireturn // data will vary for each validation
+	return verCheck
+}
+
+type VersionWithError struct {
+	Error   error  `json:"fetchError"`
+	Version string `json:"version"`
+}
+
+func MarshalVersionAndError(ver *VersionWithError) ([]byte, error) {
+	var err any
+	if ver.Error != nil {
+		err = ver.Error.Error()
+	}
+	marsh, marshalErr := json.Marshal(&struct {
+		Error   any    `json:"fetchError"`
+		Version string `json:"version"`
+	}{
+		Version: ver.Version,
+		Error:   err,
+	})
+	return marsh, fmt.Errorf("failed to marshal VersionWithError %w", marshalErr)
+}
+
+type VersionWithErrorCheck struct {
+	Error error
+	VersionCheck
+}
+
+func (verCheck *VersionWithErrorCheck) MarshalJSON() ([]byte, error) {
+	return MarshalVersionAndError(&VersionWithError{
+		Version: verCheck.Version,
+		Error:   verCheck.Error,
+	})
+}
