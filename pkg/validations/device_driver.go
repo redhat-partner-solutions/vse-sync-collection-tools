666050431012103f42cb7de6d2f9ad4face73c20
Charlie Wheeler-Robinson <cwheeler@redhat.com>
2023-08-21 11:00:19 -0400

    allow well-formed versions for out-of-tree drivers
    Previous development has been completed against out-of-tree drivers
    with 4-segment versions (e.g. v1.11.20.7). A new out-of-tree
    driver with a 3-segment version (v1.12.6) has been released, breaking
    assumptions made in the previous validation code.
    
    This change allows the new (and future) well-formed version strings
    to pass environment validation, while triggering a warning if
    a version lower than the expected in-tree version is found.
    
    Signed-off-by: Charlie Wheeler-Robinson <cwheeler@redhat.com>

--- pkg/validations/device_driver.go
+++ pkg/validations/device_driver.go
@@ -17,17 +17,26 @@ const (
 )
 
 var (
-	MinDriverVersion           = "5.14.0"
+	minDriverVersion       = "1.11.0"
+	minInTreeDriverVersion = "5.14.0"
+
 	outOfTreeIceDriverSegments = 3
 )
 
 func NewDeviceDriver(ptpDevInfo *devices.PTPDeviceInfo) *VersionWithErrorCheck {
 	var err error
 	ver := fmt.Sprintf("v%s", ptpDevInfo.DriverVersion)
-	if !semver.IsValid(ver) {
+	if semver.IsValid(ver) {
+		if semver.Compare(ver, fmt.Sprintf("v%s", minInTreeDriverVersion)) < 0 {
+			err = fmt.Errorf(
+				"found device driver version %s. This is below minimum version %s so likely an out of tree driver",
+				ptpDevInfo.DriverVersion, minInTreeDriverVersion,
+			)
+		}
+	} else {
 		if strings.Count(ptpDevInfo.DriverVersion, ".") == outOfTreeIceDriverSegments {
 			err = fmt.Errorf(
-				"unable to parse version (%s), likely an out of tree driver",
+				"unable to parse device driver version (%s), likely an out of tree driver",
 				ptpDevInfo.DriverVersion,
 			)
 		}
@@ -38,7 +47,7 @@ func NewDeviceDriver(ptpDevInfo *devices.PTPDeviceInfo) *VersionWithErrorCheck {
 			id:           deviceDriverVersionID,
 			Version:      ptpDevInfo.DriverVersion,
 			checkVersion: ptpDevInfo.DriverVersion,
-			minVersion:   MinDriverVersion,
+			minVersion:   minDriverVersion,
 			description:  deviceDriverVersionDescription,
 		},
 		Error: err,
